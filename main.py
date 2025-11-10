4127ce96dfdcd857ad61736baeae5ab38e2c30e7
corinagarcia04 <corysabel2017@gmail.com>
2025-11-10 16:06:15 -0600

    estructura general completa (validaciones pendientes)

--- main.py
+++ main.py
@@ -64,7 +64,126 @@ def formatear_personajes(personajes, peliculas):
         personajes_formateados.append(personaje_formateado)
     return personajes_formateados
 
+def consultas_web(opcion):
+    if opcion == '1':
+        nom = input("Ingrese el nombre de la película a consultar\n(Sugerencias: Castle in the Sky, Grave of the Fireflies): ")
+        # pendiente
+    elif opcion == '2':
+        pass
+    elif opcion == '3':
+        pass
+    elif opcion == '4':
+        pass
+    elif opcion == '5':
+        pass
+    else:
+        print("Opción inválida, intente de nuevo.")
+
+def consultas_registros(opcion, peliculas, personajes):
+    if opcion == '1':
+        while True: 
+            print("""MENU - CONSULTAS DE PELÍCULAS
+            1. Buscar por nombre
+            2. Buscar por director
+            3. Buscar por año de lanzamiento
+            4. Buscar por rango de duración
+            5. Buscar por calificación mínima
+            6. Regresar al menú de consultas de registros""")
+            subop = input("Seleccione una opción: ")
+            if subop == '6':
+                break
+            else:
+                consultas_peliculas(subop, peliculas)
+    elif opcion == '2':
+        while True:
+            print("""MENU - CONSULTAS DE PERSONAJES
+        1. Buscar por nombre
+        2. Buscar por género
+        3. Buscar por color de ojos
+        4. Buscar por color de cabello
+        5. Regresar al menú de consultas de registros""")
+            subop = input("Seleccione una opción: ")
+            if subop == '5':
+                break
+            else:
+                consultas_personajes(subop, personajes)
+    else:
+        print("Opción inválida, intente de nuevo.")    
+
+def consultas_peliculas(opcion, peliculas):
+    while True:
+        # Buscar por nombre
+        if opcion == '1':
+            nom = input("Ingrese el nombre de la película a buscar: ")
+        # Buscar por director
+        elif opcion == '2':
+            pass
+        # Buscar por año de lanzamiento
+        elif opcion == '3':
+            pass
+        # Buscar por rango de duración
+        elif opcion == '4':
+            pass
+        # Buscar por calificación mínima
+        elif opcion == '5':
+            pass
+        else: 
+            print('Opción inválida. Intente de nuevo')
+        respuesta = input("¿Desea hacer otra consulta de películas? (s para sí, cualquier otra tecla para regresar): ")
+        if respuesta.lower() != 's':
+            break
+
+def consultas_personajes(opcion, personajes):
+    # Buscar por nombre
+    if opcion == '1':
+        pass
+    # Buscar por género
+    elif opcion == '2':
+        pass
+    # Buscar por color de ojos
+    elif opcion == '3':
+        pass
+    # Buscar por color de cabello
+    elif opcion == '4':
+        pass
+    else:
+        print('Opción inválida. Intente de nuevo')
+
+def estadisticas(opcion, peliculas, personajes):
+    # Promedios generales (duración y calificación)
+    if opcion == '1':
+        pass
+    # Top 5 películas mejor calificadas
+    elif opcion == '2':
+        pass
+    # Porcentaje de personajes por género
+    elif opcion == '3':
+        pass
+    # Características físicas más comunes
+    elif opcion == '4':
+        pass
+    # Colores de cabello más comunes
+    elif opcion == '5':
+        pass
+    else:
+        print("Opción inválida, intente de nuevo.")
+    
+
+def graficas(opcion, peliculas, personajes):
+    # Películas por director
+    if opcion == '1':
+        pass
+    # Personajes por género
+    elif opcion == '2':
+        pass
+    # Evolución de calificaciones
+    elif opcion == '3':
+        pass
+    else:
+        print("Opción inválida, intente de nuevo.")
+
 def main():
+    # Inicializando
     api = ghibli.GhibliAPI()
     # Consultar datos completos de API
     pel = api.lista_peliculas()
@@ -72,4 +191,72 @@ def main():
     # Listas finales formateadas
     peliculas = formatear_peliculas(pel, per)
     personajes = formatear_personajes(per, pel)
+    # Menu principal 
+    print("""STUDIO GHIBLI API
+          Consulta datos y estadísticas acerca de las películas y personajes del Studio Ghibli.""")
+    while True:
+        print("""MENÚ PRINCIPAL
+            1. Consultas web
+            2. Consultas de registros
+            3. Estadísticas
+            4. Gráficas
+            5. Borrar todo y finalizar""")
+        op = input("Seleccione una opción: ")
+        if op == '1':
+            while True:
+                print("""MENU - CONSULTAS WEB
+            1. Consultar películas
+            2. Consultar personajes
+            3. Mostrar consultas de películas
+            4. Mostrar consultas de personajes
+            5. Mostrar todas las consultas
+            6. Regresar al menú principal""")
+                subop = input("Seleccione una opción: ")
+                if subop == '6':
+                    break
+                else:
+                    consultas_web(subop)
+        elif op == '2':
+            while True:
+                print("""MENU - CONSULTAS DE REGISTROS
+                    1. Consultar películas
+                    2. Consultar personajes
+                    3. Regresar al menú principal""")
+                subop = input("Seleccione una opción: ")
+                if subop == '3':
+                    break
+                else:
+                    consultas_registros(subop,peliculas,personajes)
+        elif op == '3':
+            while True:
+                print("""MENU - ESTADÍSTICAS
+                    1. Promedios generales (duración y calificación)
+                    2. Top 5 películas mejor calificadas
+                    3. Porcentaje de personajes por género
+                    4. Características físicas más comunes
+                    5. Colores de cabello más comunes
+                    6. Regresar al menú principal""")
+                subop = input("Seleccione una opción: ")
+                if subop == '6':
+                    break
+                else:
+                    estadisticas(subop, peliculas, personajes)
+        elif op == '4':
+            while True:
+                print("""MENU - GRÁFICAS
+                    1. Películas por director
+                    2. Personajes por género
+                    3. Evolución de calificaciones
+                    4. Regresar al menú principal""")
+                subop = input("Seleccione una opción: ")
+                if subop == '4':
+                    break
+                else:
+                    graficas(subop, peliculas, personajes)
+        elif op == '5':
+            # aqui falta el codigo para borrar los registros
+            break
+        else:
+            print("Opción inválida, intente de nuevo.")
+
 main()
